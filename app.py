e817bdbcdf107d1d7dc32ce2e53481c0ed6b5399
ctlee <ctlee@syncace.co>
2021-01-11 18:01:23 +0800

    0111

--- app.py
+++ app.py
@@ -56,11 +56,11 @@ def handle_follow(event):
         )
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=event.message.text))
 
 
 # if __name__ == "__main__":
